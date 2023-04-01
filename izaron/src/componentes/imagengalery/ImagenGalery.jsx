30726d9c64bff26e55328e9989fec9f6d8add0c5
GusFU <guscp84@gmail.com>
2023-04-01 19:23:54 +0200

    updates: create crousel and update image 3 secons

--- izaron/src/componentes/imagengalery/ImagenGalery.jsx
+++ izaron/src/componentes/imagengalery/ImagenGalery.jsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export const ImageGallery = () => {
   const images = Object.values(
     import.meta.globEager('../../assets/imagenes/*.{png,jpg,jpeg,gif,svg,jfif}')
   );
+  
  
+
+
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplaySpeed:3000,
+    autoplay: true,
+    
+  };
+
   return (
-    <div>
+    <Slider {...settings}>
       {images.map((image, index) => (
-        <img key={index} src={image.default} alt={`image-${index}`} />
+        <div key={index}>
+          <img src={image.default} alt={`image-${index}`} />
+        </div>
       ))}
-    </div>
+    </Slider>
   );
 };
-
